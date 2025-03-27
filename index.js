36c54066a9470a169a58df494cdcde444b7da063
HamzaAliKundi <hamza.alee83@gmail.com>
2025-03-28 00:57:11 +0500

    add origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5000", "https://universal-translator-fe.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5000", "https://universal-translator-fe.vercel.app", "https://universal-translator-steel.vercel.app"],
     credentials: true,
   })
 );
